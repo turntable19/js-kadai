693ab078be148635e220b7b6f14f44b6617c06b9
yugo-ibuki <y.ibuki91@gmail.com>
2023-06-11 21:14:16 +0900

    refactor

--- 6_08.js
+++
@@ -1,8 +0,0 @@
-let target = document.getElementById('text01');
-target.addEventListener('blur', function() {
-  let span = document.getElementById('msg');
-  if (this.value !== '') {
-    span.textContent = '入力された文字: ' + this.value;
-    span.classList.remove('hidden');
-  }
-});
