c10c30129e94b23767ffbe0b7d7c0ac4a7ddb8d6
turntable19 <minatomina811@gmail.com>
2023-06-18 14:41:46 +0900

    saiteisyutu

--- kadai9/6_04.js
+++ kadai9/6_04.js
@@ -1,20 +1,3 @@
-let main_category = ['WOMAN','MEN','KIDS','BABY'];
-
-main_category.forEach(function(e){
-  console.log(e);
-});
-
-//一緒
-let main_category2 = ['WOMAN','MEN','KIDS','BABY'];
-
-main_category.forEach(
-  // アロー関数
-  (e) => {
-    console.log(e);
-  }
-);
-
-
 let a = Array.from({length: 100}, (_, i) => i + 1);
 
 let b3 = a.filter(e => e % 3 === 0);
